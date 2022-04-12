71d2399a0a5958348e555183d2060010a5187820
josephmora <jhos92@hotmail.com>
2022-04-11 22:53:47 -0600

    Se crean los DTO y se implementanant con los datos necesarios para el front

---
+++ src/main/java/com/joseph/proyecto/integrador/modelo/PacienteDTO.java
@@ -0,0 +1,69 @@
+package com.joseph.proyecto.integrador.modelo;
+
+import java.time.LocalDate;
+
+public class PacienteDTO {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String dni;
+    private LocalDate fechaIngreso;
+    private Domicilio domicilio;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public LocalDate getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(LocalDate fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public Domicilio getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(Domicilio domicilio) {
+        this.domicilio = domicilio;
+    }
+}
---
+++ src/main/java/com/joseph/proyecto/integrador/modelo/TurnoDTO.java
@@ -0,0 +1,15 @@
+package com.joseph.proyecto.integrador.modelo;
+
+import java.time.LocalDate;
+
+public class TurnoDTO {
+    private LocalDate fecha;
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+}
